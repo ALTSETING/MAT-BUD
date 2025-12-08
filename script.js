b243e3cadb5e4739bdea356da650384327eae251
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2025-12-08 14:11:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function openTab(tabName) {
     document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
     document.getElementById(tabName).classList.add("active");
+
+    // Закриває меню на телефоні після вибору
+    document.getElementById("menu").style.display = "none";
 }
 
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
