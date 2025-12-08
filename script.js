466f3dcee635b6870db4af58dbb4512761e6ecda
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2025-12-08 15:37:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function openTab(tabName) {
+    // Переключення вкладок
     document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
     document.getElementById(tabName).classList.add("active");
 
-    // Закриває меню на телефоні після вибору
-    document.getElementById("menu").style.display = "none";
+    const menu = document.getElementById("menu");
+
+    // Якщо мобільний — ховаємо меню
+    if (window.innerWidth < 600) {
+        menu.style.display = "none";
+    } 
+    else {
+        // Якщо ПК — меню завжди видно
+        menu.style.display = "flex";
+    }
 }
 
 function toggleMenu() {
