40cb97426038fc7414069337601174dcd0501f9c
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2025-12-09 19:31:25 +0100

    Add lightbox functionality for image display

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ const afterWrapper = document.querySelector(".after-img-wrapper");
 slider.addEventListener("input", function () {
     afterWrapper.style.width = this.value + "%";
 });
+
+function openLightbox(src) {
+    const lightbox = document.getElementById("lightbox");
+    const img = document.getElementById("lightbox-img");
+
+    img.src = src;
+    lightbox.style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
